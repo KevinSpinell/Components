238b81c3eb0f21b7ee1268d959427808629f9652
KevinSpinell <kevinnallicarrascal@gmail.com>
2024-05-15 13:07:02 -0500

    XD

---
+++ src/components/vue/Directives.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { ref } from 'vue';
+    let msg = ref('Soy un tooltip'); 
+    let checked = ref(false)
+    let data = ["Hola", "Mundo", "Bonito"]
+    let filtered = data.filter((word) => word.includes('M'))
+    let filterede = data.filter(function (word) { 
+        return word.includes('B')
+    })
+</script>
+
+<template>
+    <input 
+        class="border-2 "
+        v-model="msg"
+        :class="{'border-red-500':checked}"
+    >
+    <div :title="msg">
+        {{ msg }}
+    </div>
+    <label for="check">Seleccionado {{ checked }}</label>
+    <input type="checkbox" v-model="checked"/>
+
+    <ul>
+        <li :id ="index" class="text-blue-500" v-for="(text, index) in data" :key="index"> {{(index+1) +" " + text }}</li>
+        <li :id ="index" class="text-red-500" v-for="(text, index) in filtered" :key="index"> {{(index+1) +" " + text }}</li>
+        <li :id ="index" class="text-green-500" v-for="(text, index) in filterede" :key="index"> {{(index+1) +" " + text }}</li>
+    </ul>
+
+</template>
---
+++ src/components/vue/TODO.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { ref } from 'vue'
+    const tasks = ref([]);
+    const inputTasks = ref();
+    function addTask(){
+        tasks.value.push(inputTasks.value)
+        inputTasks.value = ''
+        console.log(tasks)
+    }
+</script>
+<template>
+    <div class="max-w-96 bg-slate-300 rounded shadow mx-auto mt-10  py-8 px-10">
+        <h1 class="text-center text-lg font-semibold">
+            Lista de Tareas
+        </h1>
+        <div class="mt-4 mb-2">
+            <input type="text" class="w-4/5" v-model="inputTasks">
+            <button class="w-1/5 bg-green-500 text-white font-bold" @click="addTask()">+</button>
+        </div>
+        <div 
+            class="bg-white/60 px-2 py-2"
+            v-for="(task, index) in tasks" :key="index"
+        >
+            {{task}}
+        </div>
+    </div>
+</template>
